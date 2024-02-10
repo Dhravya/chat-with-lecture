63aef9cc486b2e5b8dc006ac0e40155a41c3de5a
Dhravya <dhravyashah@gmail.com>
2024-02-10 16:48:45 -0700

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.11.3
      project name = study-ai
      framework = next
      framework cli = create-next-app@14.1.0
      package manager = npm@10.2.4
      wrangler = wrangler@3.28.1
      git = 2.43.0

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,23 @@
+// Next.js Edge API Route Handlers: https://nextjs.org/docs/app/building-your-application/routing/router-handlers#edge-and-nodejs-runtimes
+
+import type { NextRequest } from 'next/server'
+
+export const runtime = 'edge'
+
+export async function GET(request: NextRequest) {
+  let responseText = 'Hello World'
+
+  // In the edge runtime you can use Bindings that are available in your application
+  // (for more details see:
+  //    - https://developers.cloudflare.com/pages/framework-guides/deploy-a-nextjs-site/#use-bindings-in-your-nextjs-application
+  //    - https://developers.cloudflare.com/pages/functions/bindings/
+  // )
+  //
+  // KV Example:
+  // const myKv = process.env.MY_KV
+  // await myKv.put('suffix', ' from a KV store!')
+  // const suffix = await myKv.get('suffix')
+  // responseText += suffix
+
+  return new Response(responseText)
+}
